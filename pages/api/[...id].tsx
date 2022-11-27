0207a7ffacb7db219ec1c77fc7fbe5910a387af8
MinHieu2811 <cuunhatnhat51@gmail.com>
2022-11-28 00:13:19 +0700

    start config id board page

--- pages/api/[...id].tsx
+++ pages/api/[...id].tsx
@@ -1,3 +1,7 @@
+import { db } from 'config/firebase';
+import { doc, getDoc } from 'firebase/firestore';
+import { GetServerSideProps, GetServerSidePropsResult } from 'next';
+import { Props, ScriptProps } from 'next/script';
 import * as React from 'react';
 
 export interface IAppProps {
@@ -9,4 +13,17 @@ export default function App (props: IAppProps) {
       
     </div>
   );
+}
+
+export const getServerSideProps: GetServerSideProps<Props, {id: string} > = async (context): Promise<GetServerSidePropsResult<Props>> => {
+    const boardId = context?.params?.id
+    const docRef = doc(db, 'boards', boardId as string)
+    const docSnap = await getDoc(docRef)
+    return {
+        props: {
+            children: {
+                
+            }
+        }
+    }
 }
